d23b4d1293c434c571a60be48c649ed95433fa79
viruj <virujthakur01@gmail.com>
2023-12-04 20:29:41 +0530

    designed the look of a recipe card

---
+++ lib/pages/recipecard.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class RecipeCard extends StatefulWidget {
+  const RecipeCard({super.key});
+
+  @override
+  State<RecipeCard> createState() => _RecipeCardState();
+}
+
+class _RecipeCardState extends State<RecipeCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(24),
+        side: BorderSide(
+          color: Colors.grey,
+          width: 1
+        )
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Ink.image(
+            image: AssetImage('assets/images/image1.jpg'),
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Recipe Title 1',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        letterSpacing: 5,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('This is the description of the recipe',
+                      style: TextStyle(
+                          fontSize: 16
+                      ),),
+                  ),
+
+                ],
+              ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(onPressed: (){}, icon: Icon(Icons.favorite)),
+            )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
