0d5acf5e5dd04d188dd8ba9989ce60f5b1e0a39e
viruj <virujthakur01@gmail.com>
2023-12-04 19:25:27 +0530

    Added image carousel on the home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,44 @@
+import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
 import 'package:flutter/material.dart';
 class UserHome extends StatelessWidget {
   const UserHome({super.key});
+  static const List<String> sampleImages = [
+    'assets/images/image1.jpg',
+    'assets/images/image2.jpg',
+    'assets/images/image3.jpg',
+    'assets/images/image4.jpg',
+    'assets/images/image5.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Welcome To Ingredient Inspired Recipe Recommender : Your Culinary Guide!',
-        style: TextStyle(
-            fontSize: 40
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            FanCarouselImageSlider(
+              imagesLink: sampleImages,
+              isAssets: true,
+              autoPlay: true,
+              userCanDrag: true,
+              sliderHeight: 300,
+              indicatorActiveColor: Colors.amberAccent,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Text('Ingredient Inspired Recipe Recommender, Your Culinary Guide!',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
